3b3fcc90ef8e4caabbe8e6acc927d35d19f9ee16
jtotty <james.totty1991@gmail.com>
2023-10-04 22:12:55 +0100

    Added go and rust main files. Submarine advent of code in TS.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+fn error_me(throw: bool) -> Result<(), usize> {
+    if throw {
+        return Err(7);
+    }
+
+    return Ok(());
+}
+
+fn main() -> Result<(), usize> {
+    error_me(false)?;
+
+    return Ok(());
 }
